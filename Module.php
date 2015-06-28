8c871f4ce44463040884a8f27736c70d60dcf98d
Loveorigami <loveorigami@mail.ru>
2015-06-28 12:21:13 +0300

    module config

--- Module.php
+++ Module.php
@@ -11,5 +11,7 @@ class Module extends \yii\base\Module
         parent::init();
 
         // custom initialization code goes here
+		// initialize the module with the configuration loaded from config.php
+        //\Yii::configure($this, require(__DIR__ . '/config.php'));
     }
 }
