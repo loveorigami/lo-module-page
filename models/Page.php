b79b0d03e3938ccf97ba3c5fbe9ce6efacd21c01
Loveorigami <loveorigami@mail.ru>
2016-08-17 18:58:08 +0300

    del constraint trait

--- models/Page.php
+++ models/Page.php
@@ -3,7 +3,6 @@
 namespace lo\modules\page\models;
 
 use Yii;
-use lo\modules\import\models\ICsvImportable;
 
 
 /**
@@ -18,11 +17,8 @@ use lo\modules\import\models\ICsvImportable;
  * @property integer $created_at
  * @property integer $updated_at
  */
-class Page extends \lo\core\db\ActiveRecord implements ICsvImportable
+class Page extends \lo\core\db\ActiveRecord
 {
-
-    use \lo\core\rbac\ConstraintTrait;
-
     const STATUS_DRAFT = 0;
     const STATUS_PUBLISHED = 1;
 
@@ -42,40 +38,4 @@ class Page extends \lo\core\db\ActiveRecord implements ICsvImportable
         return PageMeta::class;
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function attributeLabels()
-    {
-        return [
-            'id' => Yii::t('common', 'ID'),
-            'slug' => Yii::t('common', 'Slug'),
-            'name' => Yii::t('common', 'Name'),
-            'text' => Yii::t('common', 'Text'),
-            'status' => Yii::t('common', 'Active'),
-            'author_id' => Yii::t('common', 'Author'),
-            'updater_id' => Yii::t('common', 'Updater'),
-            'created_at' => Yii::t('common', 'Created At'),
-            'updated_at' => Yii::t('common', 'Updated At'),
-        ];
-    }
-
-    /**
-     * Возвращает массив атрибутов доступных для импорта из csv
-     * @return array
-     */
-    public function getCsvAttributes()
-    {
-        $attrs = array_keys($this->getAttributes( null, ['updated_at', 'updater_id'])); // пропустить
-       // $attrs[] = "id";
-        //$attrs[] = "confirm_password";
-        return $attrs;
-
-    }
-
-    public function getCsvCallbacks(){
-        return  [
-            'status' => 'iStatus'
-        ];
-    }
 }
